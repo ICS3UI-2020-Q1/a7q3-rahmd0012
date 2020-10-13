5fedc0bfab76a3f87c3f842ebc9f1401fd09e205
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-13 15:28:03 +0000

    asked the user for a number and printed out random number of asterisks in those lines

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that produced a random number of asteriks from 1 to 5
+ * @author Daniel Rahmani
  */
 public class Main {
+  
+
+  public static void chaotic(int lines){
+    for(int i = 0; i < lines; i++){
+      System.out.println();
+      int randNum = (int)(Math.random()*(5 - 1 + 1))+ 1;
+      for(int j = 0; j < randNum; j++){
+        System.out.print("*");
+      }
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user how many lines they want
+    System.out.println("Please enter the number of lines you want");
+    int lines = input.nextInt();
+    chaotic(lines);
+
     
   }
 }
